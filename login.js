53f6acb7f1a3e79920f014d0e24834abdd8406ab
Hugo Kaua <hugokaua2002@gmail.com>
2023-08-08 10:50:39 -0300

    commit

---
+++ login.js
@@ -0,0 +1,6 @@
+7.	Crie uma função javascript que lê o texto de um campo input e, 
+em um alert, imprime uma mensagem informando se o texto obtido do 
+campo forma um palíndromo. Se o texto tiver mais do que uma palavra, 
+você deve considerar apenas a primeira palavra ( token) da string. 
+Mostre também o HTML e considere que a função é chamada quando o input perde o foco 
+(evento onblur). (1 ponto)
